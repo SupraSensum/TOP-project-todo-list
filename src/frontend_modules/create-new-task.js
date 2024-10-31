0962c2f448b8ff99220ccc41e82ea5e65424cef2
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2024-11-01 00:18:38 +0800

    add form

--- src/frontend_modules/create-new-task.js
+++ src/frontend_modules/create-new-task.js
@@ -5,7 +5,6 @@ export default function () {
       createBlurLayer(9998);
       const newTaskBox = document.createElement('div');
       newTaskBox.id = "newTaskBox";
-      newTaskBox.textContent = "create new task";
       newTaskBox.style.cssText = `
          width: 300px;
          height: 400px;
@@ -18,7 +17,23 @@ export default function () {
          border-radius: 5px;
          border: 1px solid black;
          z-index: 9999;
+         display: flex;
+         flex-direction: column;
+         align-items: center;
+         gap: 10px;
+         font-size: 16px;
+         font-weight: 600;
+         color: #333;
       `;
+
+      const header = document.createElement('h2');
+      header.textContent = 'Create New Task';
+      header.style.cssText = `
+         margin-top: 20px;
+      `;
+      newTaskBox.appendChild(header);
+      
+      newTaskBox.appendChild(createForm());
    
       document.body.appendChild(newTaskBox);
    
@@ -26,4 +41,138 @@ export default function () {
    } else {
       console.log('new task already exists');
    }
+}
+
+function createForm() {
+   const form = document.createElement('form');
+   form.id = "newTaskForm";
+   form.onsubmit = (e) => {
+      e.preventDefault();
+      const formData = new FormData(form);
+      const title = formData.get('title');
+      const description = formData.get('description');
+      const dueDate = formData.get('dueDate');
+      const priority = formData.get('priority');
+      const notes = formData.get('notes');
+      const checklist = formData.get('checklist');
+      const projects = formData.getAll('projects');
+      const completed = formData.get('completed');
+
+      createTask({
+         title,
+         description,
+         dueDate,
+         priority,
+         notes,
+         checklist,
+         projects,
+         completed,
+      })
+   };
+   form.style.cssText = `
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 20px;
+      gap: 10px;
+      font-family: sans-serif;
+      font-size: 16px;
+      color: #333;
+      overflow: auto;
+      border: 3px solid red;
+   `;
+   const titleInput = document.createElement('input');
+   titleInput.type = 'text';
+   titleInput.name = 'title';
+   titleInput.placeholder = 'title';
+   titleInput.style.cssText = `
+      width: 100%;
+      height: 40px;
+      padding: 10px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+   `;
+
+   const descriptionInput = document.createElement('textarea');
+   descriptionInput.name = 'description';
+   descriptionInput.placeholder = 'description';
+   descriptionInput.style.cssText = `
+      width: 100%;
+      height: 100px;
+      padding: 10px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+   `;
+
+   const dueDateInput = document.createElement('input');
+   dueDateInput.type = 'date';
+   dueDateInput.name = 'dueDate';
+   dueDateInput.style.cssText = `
+      width: 100%;
+      height: 40px;
+      padding: 10px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+   `;
+
+   const priorityInput = document.createElement('input');
+   priorityInput.type = 'number';
+   priorityInput.name = 'priority';
+   priorityInput.style.cssText = `
+      width: 100%;
+      height: 40px;
+      padding: 10px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+   `;
+
+   const notesInput = document.createElement('textarea');
+   notesInput.name = 'notes';
+   notesInput.placeholder = 'notes';
+   notesInput.style.cssText = `
+      width: 100%;
+      height: 100px;
+      padding: 10px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+   `;
+
+   const checklistInput = document.createElement('textarea');
+   checklistInput.name = 'checklist';
+   checklistInput.placeholder = 'checklist';
+   checklistInput.style.cssText = `
+      width: 100%;
+      height: 100px;
+      padding: 10px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+   `;
+
+   const projectsInput = document.createElement('input');
+   projectsInput.type = 'text';
+   projectsInput.name = 'projects';
+   projectsInput.placeholder = 'projects';
+   projectsInput.style.cssText = `
+      width: 100%;
+      height: 40px;
+      padding: 10px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+   `;
+
+   const completedInput = document.createElement('input');
+   completedInput.type = 'checkbox';
+   completedInput.name = 'completed';
+
+   form.appendChild(titleInput);
+   form.appendChild(descriptionInput);
+   form.appendChild(dueDateInput);
+   form.appendChild(priorityInput);
+   form.appendChild(notesInput);
+   form.appendChild(checklistInput);
+   form.appendChild(projectsInput);
+   form.appendChild(completedInput);
+   form.appendChild(document.createElement('button'), { type: 'submit' });
+
+   return form;
 }
