991ff1f634838ce4c01a7ba78b4a0e94ed66723f
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2024-11-04 20:02:23 +0800

    refactor css styles into dedicated .css file and clean up some things

--- src/frontend_modules/create-new-task.js
+++ src/frontend_modules/create-new-task.js
@@ -1,5 +1,6 @@
 import BlurLayer from "./blur-layer.js";
 import TaskHandler from "../backend_modules/task-handler";
+import "./create-new-task.css";
 
 export default function () {
    createNewTaskBox();
@@ -8,39 +9,20 @@ export default function () {
 function createNewTaskBox() {
    if (!document.getElementById('newTaskBox')) {
       BlurLayer.create(9998, "overlay");
+
       const newTaskBox = document.createElement('div');
       newTaskBox.id = "newTaskBox";
-      newTaskBox.style.cssText = `
-         width: 300px;
-         height: 400px;
-         background-color: #BCEBCB;
-         top: 50%;
-         left: 50%;
-         transform: translate(-50%, -50%);
-         position: fixed;
-         box-shadow: 0 5px 10px 0 rgba(0,0,0,0.4);
-         border-radius: 5px;
-         border: 1px solid black;
-         z-index: 9999;
-         display: flex;
-         flex-direction: column;
-         align-items: center;
-         gap: 10px;
-         font-size: 16px;
-         font-weight: 600;
-         color: #333;
-      `;
 
       const header = document.createElement('h2');
       header.textContent = 'Create New Task';
-      header.style.cssText = `
-         margin-top: 20px;
-      `;
 
       document.addEventListener('keydown', handleEscapeKey);
-      newTaskBox.appendChild(createCloseButton());
-      newTaskBox.appendChild(header);
-      newTaskBox.appendChild(createForm());
+
+      newTaskBox.append(
+         createCloseButton(),
+         header,
+         createTaskForm()
+      );
       document.body.appendChild(newTaskBox);
    } else {
       console.log('new task already exists');
@@ -55,149 +37,90 @@ function handleEscapeKey(e) {
 
 function createCloseButton() {
    const closeButton = document.createElement('button');
+   closeButton.id = "closeButton";
    closeButton.textContent = 'X';
-   closeButton.style.cssText = `
-      position: absolute;
-      top: 2px;
-      left: 5px;
-      background-color: transparent;
-      color: #333;
-      border: none;
-      padding: 10px;
-      cursor: pointer;
-      font-size: 16px;
-      font-weight: 600;
-   `;
    closeButton.onclick = closeNewTaskBox;
    return closeButton;
 }
 
-function createForm() {
+function getFormData(form) {
+   const formData = Object.fromEntries(new FormData(form));
+   const {
+      title,
+      description,
+      dueDate,
+      priority,
+      notes,
+      checklist,
+      projects,
+      completed,
+   } = formData;
+
+   return {
+      title,
+      description: description || '',
+      dueDate,
+      priority,
+      notes: notes || '',
+      checklist: checklist || '',
+      projects: projects.split(','),
+      completed,
+   };
+}
+
+function createTaskForm() {
    const taskHandler = new TaskHandler();
+
    const form = document.createElement('form');
    form.id = "newTaskForm";
    form.onsubmit = (e) => {
       e.preventDefault();
-      const formData = Object.fromEntries(new FormData(form));
-      const {
-         title,
-         description,
-         dueDate,
-         priority,
-         notes,
-         checklist,
-         projects,
-         completed,
-      } = formData;
-
-      taskHandler.createTask({
-         title,
-         description: description || '',
-         dueDate,
-         priority,
-         notes: notes || '',
-         checklist: checklist || '',
-         projects: projects.split(','),
-         completed,
-      })
+      taskHandler.createTask(getFormData(form));
 
       form.reset();
       closeNewTaskBox();
    };
-   form.style.cssText = `
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding: 20px;
-      gap: 10px;
-      font-family: sans-serif;
-      font-size: 16px;
-      color: #333;
-      overflow: auto;
-      width: 100%;
-   `;
+
    const titleInput = document.createElement('input');
    titleInput.type = 'text';
    titleInput.name = 'title';
    titleInput.placeholder = 'title';
    titleInput.required = true;
-   titleInput.style.cssText = `
-      width: 100%;
-      height: 40px;
-      padding: 10px;
-      border-radius: 5px;
-      border: 1px solid #ccc;
-   `;
+   titleInput.classList.add('normalFormField');
 
    const descriptionInput = document.createElement('textarea');
    descriptionInput.name = 'description';
    descriptionInput.placeholder = 'description';
-   descriptionInput.style.cssText = `
-      width: 100%;
-      height: 100px;
-      padding: 10px;
-      border-radius: 5px;
-      border: 1px solid #ccc;
-   `;
+   descriptionInput.classList.add('tallerFormField');
 
    const dueDateInput = document.createElement('input');
    dueDateInput.type = 'date';
    dueDateInput.name = 'dueDate';
    dueDateInput.required = true;
-   dueDateInput.style.cssText = `
-      width: 100%;
-      height: 40px;
-      padding: 10px;
-      border-radius: 5px;
-      border: 1px solid #ccc;
-   `;
+   dueDateInput.classList.add('normalFormField');
 
    const priorityInput = document.createElement('input');
    priorityInput.type = 'number';
    priorityInput.name = 'priority';
    priorityInput.required = true;
-   priorityInput.style.cssText = `
-      width: 100%;
-      height: 40px;
-      padding: 10px;
-      border-radius: 5px;
-      border: 1px solid #ccc;
-   `;
+   priorityInput.classList.add('normalFormField');
 
    const notesInput = document.createElement('textarea');
    notesInput.name = 'notes';
    notesInput.placeholder = 'notes';
-   notesInput.style.cssText = `
-      width: 100%;
-      height: 100px;
-      padding: 10px;
-      border-radius: 5px;
-      border: 1px solid #ccc;
-   `;
+   notesInput.classList.add('tallerFormField');
 
    const checklistInput = document.createElement('textarea');
    checklistInput.name = 'checklist';
    checklistInput.placeholder = 'checklist';
-   checklistInput.style.cssText = `
-      width: 100%;
-      height: 100px;
-      padding: 10px;
-      border-radius: 5px;
-      border: 1px solid #ccc;
-   `;
+   checklistInput.classList.add('tallerFormField');
 
    const projectsInput = document.createElement('input');
    projectsInput.type = 'text';
    projectsInput.name = 'projects';
    projectsInput.placeholder = 'projects';
    projectsInput.required = true;
-   projectsInput.style.cssText = `
-      width: 100%;
-      height: 40px;
-      padding: 10px;
-      border-radius: 5px;
-      border: 1px solid #ccc;
-   `;
+   projectsInput.classList.add('normalFormField');
 
    const completedInput = document.createElement('input');
    completedInput.type = 'checkbox';
@@ -206,26 +129,18 @@ function createForm() {
    const submitButton = document.createElement('button');
    submitButton.type = 'submit';
    submitButton.textContent = 'Submit';
-   submitButton.style.cssText = `
-      width: 100%;
-      height: 40px;
-      padding: 10px;
-      border-radius: 5px;
-      border: 1px solid #ccc;
-      background-color: #4CAF50;
-      color: white;
-      cursor: pointer;
-   `;
 
-   form.appendChild(titleInput);
-   form.appendChild(descriptionInput);
-   form.appendChild(dueDateInput);
-   form.appendChild(priorityInput);
-   form.appendChild(notesInput);
-   form.appendChild(checklistInput);
-   form.appendChild(projectsInput);
-   form.appendChild(completedInput);
-   form.appendChild(submitButton);
+   form.append(
+      titleInput,
+      descriptionInput,
+      dueDateInput,
+      priorityInput,
+      notesInput,
+      checklistInput,
+      projectsInput,
+      completedInput,
+      submitButton,
+   );
 
    return form;
 }
